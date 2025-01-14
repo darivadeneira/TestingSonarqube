110666274e33e054eb41a11af64d356e0b2c2199
darivadeneira <generalmayorxd@gmail.com>
2025-01-14 09:09:20 -0500

    first commit

---
+++ GestionProductos/AppDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GestionProductos
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+
+        public DbSet<Producto> Productos { get; set; }
+
+        public class Producto
+        {
+            public int Id { get; set; }
+            public string Nombre { get; set; }
+            public string Descripcion { get; set; }
+            public Decimal Precio { get; set; }
+            public int Stock { get; set; }
+        }
+    }
+}
---
+++ GestionProductos/Controllers/GestionProductos.Controllers.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static GestionProductos.AppDBContext;
+
+namespace GestionProductos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : Controller
+    {
+        private readonly AppDBContext _appDBcontext;
+        private string? password;
+
+        public ProductoController(AppDBContext appDBcontext)
+        {
+            _appDBcontext = appDBcontext;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetProductos()
+        {
+            return Ok(await _appDBcontext.Productos.ToListAsync());
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> CreateProducto(Producto producto)
+        {
+            if (producto.Precio < 0) return BadRequest("El precio no puede ser negativo");
+            _appDBcontext.Productos.Add(producto);
+            await _appDBcontext.SaveChangesAsync();
+            return Ok(producto);
+        }
+    }
+}
